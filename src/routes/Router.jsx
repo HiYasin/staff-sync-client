d96186b20c28f6449d845afa73f9ccb3f3e53c70
HiYasin <hello.mr.faisal@gmail.com>
2025-01-20 00:14:21 +0600

    Sign up page created

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -5,6 +5,7 @@ import PublicHome from "../pages/PublicHome";
 import ErrorSection7 from "../components/shared/ErrorSection7";
 import Contact from "../pages/Contact";
 import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
 const Router = createBrowserRouter([
     {
         path: "/",
@@ -22,6 +23,10 @@ const Router = createBrowserRouter([
             {
                 path: "/signin",
                 element: <SignIn></SignIn>
+            },
+            {
+                path: "/signup",
+                element: <SignUp></SignUp>
             },
             {
                 path: "/error",
