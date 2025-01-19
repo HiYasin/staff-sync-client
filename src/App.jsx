331fd3f2c3c4984e0114b23119f9f97237c5652c
HiYasin <hello.mr.faisal@gmail.com>
2025-01-19 20:18:04 +0600

    Initial project setup ready

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@material-tailwind/react"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-center">Hello world</h1>
+    </>
+  )
+}
+
+export default App
